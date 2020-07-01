283e9f519e129c5f09d34aadb20f5c5f8a39f73d
nurzaman <nurzaman@gmail.com>
2020-07-01 16:58:55 +0700

    add error handler

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,36 +1,30 @@
 package users
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/nurzamanindra/bookstore_users-api/domain/users"
 	"github.com/nurzamanindra/bookstore_users-api/services"
+	"github.com/nurzamanindra/bookstore_users-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		//TODO : Handle error bad request
+		restErr := errors.NewBadRequestError("Invalid Json Body")
 
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		//TODO: error handler
+		c.JSON(restErr.Status, restErr)
 		return
 	}
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		//TODO: Handle Json Error
-		fmt.Println(err.Error())
-		return
-	}
+
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
 		//TODO : Handle user creator error
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
-	fmt.Println(user)
-
 	c.JSON(http.StatusCreated, result)
 }
 
