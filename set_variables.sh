b9f56e6866045b12e867543ad5dd5740ddaeb101
nurzaman <nurzaman@gmail.com>
2020-07-06 18:49:57 +0700

    adding go.mod

---
+++ set_variables.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo "execute file"
+export mysql_users_username=dashboard
+export mysql_users_password=welcome1
+export mysql_users_host=127.0.0.1:3306
+export mysql_users_schema=users_db
+
